615b3c2833604cbb314eebdbd99ba0745347f8b6
MartinHolena <martinkiko18@gmail.com>
2021-03-04 15:30:34 +0100

    Add symptoms for vitamin A, B1, B2, B6, H

--- Lekarnik/Data/Symptom.cs
+++ Lekarnik/Data/Symptom.cs
@@ -14,14 +14,98 @@ namespace Lekarnik.Data
 
             Symps.Add(new Symp
             {
-                Nazov_Symptom = "Bolesť",
-                Popis_Symptom = "Dobre toto"
+                Nazov_Symptom = "Zhoršené videnie",
+                Popis_Symptom = "A pomôže"
             });
 
             Symps.Add(new Symp
             {
-                Nazov_Symptom = "mala Bolesť",
-                Popis_Symptom = "Zle toto"
+                Nazov_Symptom = "Hnačky a črevné problémy",
+                Popis_Symptom = "A pomôže"
+            });
+
+            Symps.Add(new Symp
+            {
+                Nazov_Symptom = "Koža posiatá škvrnami ",
+                Popis_Symptom = "A pomôže"
+            });
+
+            Symps.Add(new Symp
+            {
+                Nazov_Symptom = "Podráždenosť",
+                Popis_Symptom = "B1 pomôže"
+            });
+
+            Symps.Add(new Symp
+            {
+                Nazov_Symptom = "Únava ",
+                Popis_Symptom = "B1 pomôže ale H pomôže"
+            });
+
+            Symps.Add(new Symp
+            {
+                Nazov_Symptom = "Strata chute do jedla",
+                Popis_Symptom = "B1 pomôže"
+            });
+
+            Symps.Add(new Symp
+            {
+                Nazov_Symptom = "Zbúdanie",
+                Popis_Symptom = "B1 pomôže"
+            });
+
+            Symps.Add(new Symp
+            {
+                Nazov_Symptom = "Pľuzgiere a kútiky",
+                Popis_Symptom = "B2 pomôže"
+            });
+
+            Symps.Add(new Symp
+            {
+                Nazov_Symptom = "Odlupovanie kože na tvári",
+                Popis_Symptom = "B2 pomôže"
+            });
+
+            Symps.Add(new Symp
+            {
+                Nazov_Symptom = "Citlivosť na svetlo",
+                Popis_Symptom = "B2 pomôže"
+            });
+
+            Symps.Add(new Symp
+            {
+                Nazov_Symptom = "Śklbanie vo svaloch",
+                Popis_Symptom = "B2 pomôže"
+            });
+
+            Symps.Add(new Symp
+            {
+                Nazov_Symptom = "Kŕče v nohách",
+                Popis_Symptom = "B6 pomôže"
+            });
+
+            Symps.Add(new Symp
+            {
+                Nazov_Symptom = "Nízka hladina cukru v krvi",
+                Popis_Symptom = "B6 pomôže"
+            });
+
+            Symps.Add(new Symp
+            {
+                Nazov_Symptom = "Nespavosť",
+                Popis_Symptom = "H pomôže"
+            });
+
+            Symps.Add(new Symp
+            {
+                Nazov_Symptom = "Depresia",
+                Popis_Symptom = "H pomôže"
+            });
+
+            Symps.Add(new Symp
+            {
+                Nazov_Symptom = "BOlesť svalov",
+                Popis_Symptom = "H pomôže"
             });
         }
     }
