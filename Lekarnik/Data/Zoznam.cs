b87ea5c2a32a447789d736cf7878f7ce5d8fc063
MartinHolena <martinkiko18@gmail.com>
2021-03-06 16:08:07 +0100

    Add vitamins names

--- Lekarnik/Data/Zoznam.cs
+++ Lekarnik/Data/Zoznam.cs
@@ -13,13 +13,91 @@ namespace Lekarnik.Data
 
             Zoznams.Add(new Vita
             {
-                Nazov = "Acko",
+                Nazov = " Vitamín A",
                 Popis = "Skvely vitamin"
             });
 
             Zoznams.Add(new Vita
             {
-                Nazov = "Becko",
+                Nazov = "Vitamín B1",
+                Popis = "Dobry vitamin"
+            });
+           
+            Zoznams.Add(new Vita
+            {
+                Nazov = "Vitamín B2",
+                Popis = "Dobry vitamin"
+            });
+           
+            Zoznams.Add(new Vita
+            {
+                Nazov = "Vitamín B6",
+                Popis = "Dobry vitamin"
+            });
+           
+            Zoznams.Add(new Vita
+            {
+                Nazov = "Vitamín B12",
+                Popis = "Dobry vitamin"
+            });
+            
+            Zoznams.Add(new Vita
+            {
+                Nazov = "Vitamín C",
+                Popis = "Dobry vitamin"
+            });
+            
+            Zoznams.Add(new Vita
+            {
+                Nazov = "Vitamín F",
+                Popis = "Dobry vitamin"
+            });
+          
+            Zoznams.Add(new Vita
+            {
+                Nazov = "Vitamín H",
+                Popis = "Dobry vitamin"
+            });
+           
+            Zoznams.Add(new Vita
+          
+            {
+                Nazov = "Vitamín K",
+                Popis = "Dobry vitamin"
+            });
+            
+            Zoznams.Add(new Vita
+
+            {
+                Nazov = "Draslík",
+                Popis = "Dobry vitamin"
+            });
+           
+            Zoznams.Add(new Vita
+
+            {
+                Nazov = "Horčík",
+                Popis = "Dobry vitamin"
+            });
+            
+            Zoznams.Add(new Vita
+
+            {
+                Nazov = "Vápnik",
+                Popis = "Dobry vitamin"
+            });
+            
+            Zoznams.Add(new Vita
+
+            {
+                Nazov = "Zinok",
+                Popis = "Dobry vitamin"
+            });
+
+            Zoznams.Add(new Vita
+
+            {
+                Nazov = "Železo",
                 Popis = "Dobry vitamin"
             });
         }
