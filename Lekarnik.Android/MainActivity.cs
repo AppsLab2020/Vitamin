647eff51f201e50a193809edfc26fa5f593640b8
MatejSmith <mtjkovac@gmail.com>
2021-03-08 10:06:43 +0100

    make simple splash screen

--- Lekarnik.Android/MainActivity.cs
+++ Lekarnik.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Lekarnik.Droid
 {
-    [Activity(Label = "Vitamínový poradca", Icon = "@mipmap/app_icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ Lekarnik.Android/SplashActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lekarnik.Droid
+{
+    [Activity(Label = "Vitamínový poradca", Icon = "@mipmap/app_icon", Theme = "@style/Theme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            System.Threading.Thread.Sleep(1000);
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
